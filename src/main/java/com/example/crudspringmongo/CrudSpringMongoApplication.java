893592ed941d652ba4a3a9eb535ac3bf2f96071d
Julia lopes <jlr.nwb@gmail.com>
2023-01-29 16:35:45 -0300

    Initial commit

---
+++ src/main/java/com/example/crudspringmongo/CrudSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudspringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringMongoApplication.class, args);
+	}
+
+}
