83257e527944f4d18c760ad820393ed052057659
Julia lopes <jlr.nwb@gmail.com>
2023-02-04 06:53:07 -0300

    Put and Delete Mapping

--- src/main/java/com/example/crudspringmongo/models/Student.java
+++ src/main/java/com/example/crudspringmongo/models/Student.java
@@ -42,4 +42,91 @@ public class Student {
         this.totalSpentInBooks = totalSpentInBooks;
         this.created = created;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<String> getFavouriteSubjects() {
+        return favouriteSubjects;
+    }
+
+    public void setFavouriteSubjects(List<String> favouriteSubjects) {
+        this.favouriteSubjects = favouriteSubjects;
+    }
+
+    public BigDecimal getTotalSpentInBooks() {
+        return totalSpentInBooks;
+    }
+
+    public void setTotalSpentInBooks(BigDecimal totalSpentInBooks) {
+        this.totalSpentInBooks = totalSpentInBooks;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", gender=" + gender +
+                ", address=" + address +
+                ", favouriteSubjects=" + favouriteSubjects +
+                ", totalSpentInBooks=" + totalSpentInBooks +
+                ", created=" + created +
+                '}';
+    }
 }
